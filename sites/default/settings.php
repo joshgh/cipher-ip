9e9cdc9b5dcfcda0291be684e7e63da230abb9ea
Joshua Huffman <j.m.huffman@gmail.com>
2016-12-02 13:54:50 -0800

    export database, change port settings for compatibility with MAMP

--- sites/default/settings.php
+++ sites/default/settings.php
@@ -253,7 +253,7 @@ $databases = array (
       'username' => 'cipher',
       'password' => 'cipher',
       'host' => '127.0.0.1',
-      'port' => '',
+      'port' => '8889',
       'driver' => 'mysql',
       'prefix' => '',
     ),
